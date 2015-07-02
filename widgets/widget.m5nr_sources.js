0d845f2f4fd1094c7d674b37cb40ae3b3c8ab983
Tobias Paczian <paczian@mcs.anl.gov>
2015-07-02 08:00:26 -0500

    added placeolder pages

---
+++ widgets/widget.m5nr_sources.js
@@ -0,0 +1,33 @@
+(function () {
+    var widget = Retina.Widget.extend({
+        about: {
+                title: "M5NR Download Widget",
+                name: "m5nr_download",
+                author: "Tobias Paczian",
+                requires: [  ]
+        }
+    });
+    
+    widget.setup = function () {
+	return [];
+    };
+        
+    widget.display = function (params) {
+        var widget = Retina.WidgetInstances.m5nr_download[1];
+	
+	if (params && params.main) {
+	    widget.main = params.main;
+	    widget.sidebar = params.sidebar;
+	}
+	var content = widget.main;
+	widget.sidebar.parentNode.style.display = "none";
+
+	jQuery.extend(widget, params);
+	
+	var html = '\
+<h3>M5nr Data Sources</h3>\
+<p>Not yet implemented</p>';
+
+	content.innerHTML = html;
+    };
+})();
